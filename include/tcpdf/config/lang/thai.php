b80f233e0ec4131c8d07e4b716b746361e865629
Dipendra Deshar <jedeshar@gmail.com>
2017-12-25 17:10:05 +0700

    Added tcpdf with th font config

---
+++ include/tcpdf/config/lang/thai.php
@@ -0,0 +1,39 @@
+<?php
+//============================================================+
+// File name   : thai.php
+// Begin       : 2017-10-03
+// Last Update : 2017-10-03
+//
+// Description : Language module for TCPDF
+//               (contains translated texts)
+//               Thai
+//
+// Author: Dipendra Deshar
+//
+//============================================================+
+
+/**
+ * TCPDF language file (contains translated texts).
+ * @package com.tecnick.tcpdf
+ * @brief TCPDF language file: English
+ * @author Nicola Asuni
+ * @since 2004-03-03
+ */
+
+// English
+
+global $l;
+$l = Array();
+
+// PAGE META DESCRIPTORS --------------------------------------
+
+$l['a_meta_charset'] = 'UTF-8';
+$l['a_meta_dir'] = 'ltr';
+$l['a_meta_language'] = 'th';
+
+// TRANSLATIONS --------------------------------------
+$l['w_page'] = 'หน้า';
+
+//============================================================+
+// END OF FILE
+//============================================================+
